aa2a2a1bec19deed8041fb9972a0590564513c6e
Manash Swain <swainm099@gmail.com>
2025-09-10 09:53:11 +0000

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',   // for Drive direct links
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // for public Google-hosted images
       },
     ],
   },
