e2c90ecbcde0a8acb35a95693197d2c8707a0224
Manash Swain <swainm099@gmail.com>
2025-09-10 15:30:32 +0000

    https://commons.wikimedia.org/wiki/File:Microsoft_Office_Excel_(2019%E2%

--- src/components/icons/ExcelIcon.tsx
+++ src/components/icons/ExcelIcon.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function ExcelIcon() {
   return (
     <Image
-      src="https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/microsoftexcel.svg"
+      src="https://upload.wikimedia.org/wikipedia/commons/3/34/Microsoft_Office_Excel_%282019%E2%80%93present%29.svg"
       alt="Microsoft Excel Logo"
       width={40}
       height={40}
