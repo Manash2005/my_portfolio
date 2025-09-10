07f351e4955e7bb9e8a088e37a585f0b28cc8637
Manash Swain <swainm099@gmail.com>
2025-09-10 10:24:53 +0000

    fix the errors

--- src/components/icons/HtmlIcon.tsx
+++ src/components/icons/HtmlIcon.tsx
@@ -1,10 +1,12 @@
-import type { SVGProps } from "react";
+import Image from "next/image";
 
-export function HtmlIcon(props: SVGProps<SVGSVGElement>) {
+export function HtmlIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-      <path d="M15 16l-3-1-3 1 3-7 3 7z" />
-      <path d="M2 3l1.2 13.5L12 21l8.8-4.5L22 3H2z" />
-    </svg>
+    <Image
+      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+      alt="HTML5 Logo"
+      width={40}
+      height={40}
+    />
   );
 }
