d2c3b234bd56d9149fedf1c4ea8a5fa727ac2dba
Manash Swain <swainm099@gmail.com>
2025-09-10 15:26:29 +0000

    The ms excel logo is not working properly in the tech stack section , ca

--- src/components/icons/ExcelIcon.tsx
+++ src/components/icons/ExcelIcon.tsx
@@ -4,10 +4,11 @@ import Image from "next/image";
 export function ExcelIcon() {
   return (
     <Image
-      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/msexcel/msexcel-original.svg"
+      src="https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/microsoftexcel.svg"
       alt="Microsoft Excel Logo"
       width={40}
       height={40}
+      className="dark:invert"
     />
   );
 }
