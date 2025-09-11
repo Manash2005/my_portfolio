c93bfa7a999c60df6000433f527b5c312a7b7c1a
Manash Swain <swainm099@gmail.com>
2025-09-11 10:31:46 +0000

    dont do it svg , i have provided  u the image link make those images

--- src/components/icons/GeeksForGeeksIcon.tsx
+++ src/components/icons/GeeksForGeeksIcon.tsx
@@ -1,15 +1,14 @@
 
+import Image from "next/image";
+
 export function GeeksForGeeksIcon() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="40"
-      height="40"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="text-green-500"
-    >
-      <path d="M19.82 2H4.18A2.18 2.18 0 0 0 2 4.18v15.64A2.18 2.18 0 0 0 4.18 22h15.64A2.18 2.18 0 0 0 22 19.82V4.18A2.18 2.18 0 0 0 19.82 2zM9.62 17.43l-3-4.52h2.2l1.9 2.87.89 1.33.87-1.33 1.9-2.87h2.2l-3.02 4.52-1.46 2.18z" />
-    </svg>
+    <Image
+      src="https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/geeksforgeeks.svg"
+      alt="GeeksForGeeks Logo"
+      width={40}
+      height={40}
+      className="dark:invert"
+    />
   );
 }
--- src/components/icons/LeetCodeIcon.tsx
+++ src/components/icons/LeetCodeIcon.tsx
@@ -1,16 +1,14 @@
 
+import Image from "next/image";
+
 export function LeetCodeIcon() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="40"
-      height="40"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="text-yellow-400"
-    >
-      <path d="M13.48 4.26l2.12.38L12 16.11l-3.6-6.47-2.12.38 5.72 10.23z" />
-      <path d="M20.48 4.26l-8.49 15.11L3.52 4.64l2.12-.38L12 16.11l6.36-11.47z" />
-    </svg>
+    <Image
+      src="https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/leetcode.svg"
+      alt="LeetCode Logo"
+      width={40}
+      height={40}
+      className="dark:invert"
+    />
   );
 }
