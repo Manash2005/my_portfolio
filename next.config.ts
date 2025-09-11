dce64186687b9dc7d88b5feb085a6ab824df7136
Manash Swain <swainm099@gmail.com>
2025-09-11 11:28:13 +0000

    No keep the image as it is i wanted the avatar in other sections of the

--- next.config.ts
+++ next.config.ts
@@ -22,10 +22,6 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.dicebear.com',
       }
     ],
   },
